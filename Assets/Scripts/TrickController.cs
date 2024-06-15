643a6a8b7e6b16f4664c45b0d123ffd7b80d5f32
Quincy Michels <quincy.michels@gmail.com>
2024-06-15 19:06:40 -0400

    added doing multiple tricks mid air

--- Assets/Scripts/TrickController.cs
+++ Assets/Scripts/TrickController.cs
@@ -5,38 +5,65 @@ using UnityEngine;
 public class TrickController : MonoBehaviour
 {
     [SerializeField] private TrickSetSO _trickSet;
+    [SerializeField] private bool _alwaysTrick = false;
 
     private List<TrickButtons> _inputButtons = new List<TrickButtons>();
 
     private bool _canReceiveInputs = false;
+    private bool _inputGiven = false;
+    private PlayerMovement3D _movement;
     private ScoreController _scoreController;
 
     private void Awake()
     {
         _scoreController = GetComponent<ScoreController>();
+        _movement = GetComponent<PlayerMovement3D>();
     }
 
     public void SetCanTrick(bool canTrick)
     {
         _canReceiveInputs = canTrick;
+        if (!canTrick)
+        {
+            if (!CheckTrickSuccess() && _alwaysTrick && _inputButtons.Count <= 0)
+            {
+                _scoreController.BreakCombo();
+            }
+            else if (!CheckTrickSuccess() && _inputButtons.Count > 0)
+            {
+                _scoreController.BreakCombo();
+            }
+
+            _inputButtons.Clear();
+        }
+
+        _inputGiven = false;
     }
 
     public void ReceiveTrickInput(TrickButtons Button)
     {
         if (!_canReceiveInputs) return;
 
+        _inputGiven = true;
         _inputButtons.Add(Button);
+        if (!CheckTrickSuccess() && !_movement.IsFalling)
+        {
+            _inputButtons.Clear();
+            _scoreController.BreakCombo();
+        }
     }
 
     public bool CheckTrickSuccess()
     {
         // No buttons were pressed
-        if (_inputButtons.Count <= 0)
+        if (_inputButtons.Count <= 0 && !_inputGiven)
         {
+            if (_alwaysTrick) { _scoreController.BreakCombo(); }
             _inputButtons.Clear();
-            _scoreController.BreakCombo();
+
             return false;
         }
+        else if (_inputButtons.Count <= 0 && _inputGiven) return true;
 
         // For each combo in the trick set
         for (int i = 0; i < _trickSet.TrickCombos.Count; i++)
@@ -61,8 +88,6 @@ public class TrickController : MonoBehaviour
             }
         }
 
-        _inputButtons.Clear();
-        _scoreController.BreakCombo();
         return false;
     }
 }
