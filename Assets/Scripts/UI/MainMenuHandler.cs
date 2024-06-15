3f0537f3dffb6c74200448a780a523dc1644188d
Evan English <evanenglish27@gmail.com>
2024-06-14 20:11:49 -0700

    main meny

---
+++ Assets/Scripts/UI/MainMenuHandler.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    [Header ("Settings")]
+    [SerializeField] private string gameplaySceneToLoad;
+
+    [Header("References")]
+    [SerializeField] GameObject menuHandle;
+    [SerializeField] GameObject settingsMenuHandle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(gameplaySceneToLoad);
+    }
+
+    public void OpenSettingsMenu()
+    {
+        settingsMenuHandle.SetActive(true);
+        menuHandle.SetActive(false);
+    }
+
+    public void CloseSettingsMenu()
+    {
+        settingsMenuHandle.SetActive(false);
+        menuHandle.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+
+#if UNITY_EDITOR
+
+        UnityEditor.EditorApplication.isPlaying = false;
+
+#endif  
+    
+        Application.Quit();
+    }
+}
