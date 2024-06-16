6c8204b662a8702c97fc4c7b2dffd77cb592623b
navnxv <singh.navpreet14.1@gmail.com>
2024-06-16 21:40:56 +0530

    Cleaned up coin script and vfx implementation for the coins.

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -6,27 +6,20 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
-    private SphereCollider _collider;
-    private bool _hastriggered = false;
-    [SerializeField] private float rotationSpeedX = 45f;  // Degrees per second around the x-axis
-    [SerializeField] private float rotationSpeedY = 45f;  // Degrees per second around the y-axis
-    [SerializeField] private float rotationSpeedZ = 45f;  // Degrees per second around the z-axis
-    [SerializeField] private int scoreAmount = 50;
-    private Vector3 startPos;
+    [SerializeField] private float _rotationSpeedX = 45f; 
+    [SerializeField] private float _rotationSpeedY = 45f; 
+    [SerializeField] private float _rotationSpeedZ = 45f; 
+    [SerializeField] private int _scoreAmount = 50;
+    [SerializeField] private GameObject _coinVfxPrefab;
 
-    private float rotationAmountX;
-    private float rotationAmountY;
-    private float rotationAmountZ;
 
-    private void Awake()
-    {
-        _collider = GetComponent<SphereCollider>();
-    }
+    private float _rotationAmountX;
+    private float _rotationAmountY;
+    private float _rotationAmountZ;
 
     void Start()
     {
         // Record the starting position of the GameObject
-        startPos = transform.position;
         StartCoroutine(SpinLoop());
     }
 
@@ -35,29 +28,43 @@ public class Coin : MonoBehaviour
         while (true) 
         {
             // Calculate the rotation for this frame
-            rotationAmountX = rotationSpeedX * Time.deltaTime;
-            rotationAmountY = rotationSpeedY * Time.deltaTime;
-            rotationAmountZ = rotationSpeedZ * Time.deltaTime;
+            _rotationAmountX = _rotationSpeedX * Time.deltaTime;
+            _rotationAmountY = _rotationSpeedY * Time.deltaTime;
+            _rotationAmountZ = _rotationSpeedZ * Time.deltaTime;
 
             // Apply the rotations
-            transform.Rotate(Vector3.right, rotationAmountX);
-            transform.Rotate(Vector3.up, rotationAmountY);
-            transform.Rotate(Vector3.forward, rotationAmountZ);
+            transform.Rotate(Vector3.right, _rotationAmountX);
+            transform.Rotate(Vector3.up, _rotationAmountY);
+            transform.Rotate(Vector3.forward, _rotationAmountZ);
 
             yield return new WaitForSeconds(0.01f);
         }
     }
 
-
     private void OnTriggerEnter(Collider collision)
     {
-            if (collision.gameObject.TryGetComponent<PlayerMesh>(out PlayerMesh playerMesh))
+        if (collision.gameObject.TryGetComponent<PlayerMesh>(out PlayerMesh playerMesh))
+        {
+            if(playerMesh.PlayerGameObject.TryGetComponent<ScoreController>(out ScoreController scoreController))
             {
-                if(playerMesh.PlayerGameObject.TryGetComponent<ScoreController>(out ScoreController scoreController))
+                scoreController.AddCoinScore(_scoreAmount);
+
+                this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+
+                if(_coinVfxPrefab != null)
                 {
-                scoreController.AddCoinScore(scoreAmount);
-                Destroy(this.gameObject);
+                    Instantiate(_coinVfxPrefab, _coinVfxPrefab.transform);
                 }
+
+                StartCoroutine(DestroyAfterDelay(0.5f));
             }
+        }
+    }
+
+    IEnumerator DestroyAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(this.gameObject);
+        
     }
 }
