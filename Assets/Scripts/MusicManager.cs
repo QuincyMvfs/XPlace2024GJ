0fc5ae08eff48117c9fbd2ef5d6776559a75eb04
Quincy Michels <quincy.michels@gmail.com>
2024-06-17 16:15:16 -0400

    added trick sounds and music (not implemented)

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip _victoryMusic;
+    private AudioClip _currentMusic;
+    private AudioSource _source;
+
+    private void Awake()
+    {
+        _source = GetComponent<AudioSource>();
+        _source.loop = true;
+        _source.Play();
+    }
+
+    public void PlayVictoryMusic()
+    {
+        _source.clip = _victoryMusic;
+        _source.loop = false;
+        _source.Play();
+    }
+}
