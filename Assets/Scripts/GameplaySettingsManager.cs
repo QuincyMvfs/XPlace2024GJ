34c48d83b5d999641a6921de85fa0c746da53515
Quincy Michels <quincy.michels@gmail.com>
2024-06-17 13:48:38 -0400

    updated settings menu and some organization

--- Assets/Scripts/GameplaySettingsManager.cs
+++ Assets/Scripts/GameplaySettingsManager.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class GameplaySettingsManager : MonoBehaviour
 {
+    [SerializeField] private QualityLevel _defaultQualityLevel = QualityLevel.Good;
+
     private void Awake()
     {
         // Sets max fps to screen 
         RefreshRate refreshRate = Screen.currentResolution.refreshRateRatio;
         int refreshRateInt = Mathf.RoundToInt((float)refreshRate.numerator / refreshRate.denominator);
         Application.targetFrameRate = refreshRateInt;
+
+        SetGraphicSettings(_defaultQualityLevel);
+    }
+
+    public void SetGraphicSettings(QualityLevel qualityLevel)
+    {
+        QualitySettings.SetQualityLevel((int)qualityLevel);
     }
 }
